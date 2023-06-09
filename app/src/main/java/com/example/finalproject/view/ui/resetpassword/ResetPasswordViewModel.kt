ced1caf3f244b79167c2383b27ce1ebaffeedcbb
Fikri Haikal <fikrihaikal571@gmail.com>
2023-06-09 20:44:15 +0700

    ui reset password - haikal

---
+++ app/src/main/java/com/example/finalproject/view/ui/resetpassword/ResetPasswordViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.finalproject.view.ui.resetpassword
+
+import androidx.lifecycle.ViewModel
+
+class ResetPasswordViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
